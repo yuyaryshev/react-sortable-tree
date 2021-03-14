6b2c2ddcdb9df05d6aa1ba1fa8993a6bcf26e354
Yuri Yaryshev <yuyaryshev@gmail.com>
2021-03-14 15:41:01 +0300

    Repaired build for Rollup.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,42 +1,50 @@
-import nodeResolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import babel from '@rollup/plugin-babel';
+import typescript from 'rollup-plugin-typescript2';
+//import nodeResolve from '@rollup/plugin-node-resolve';
 import postcss from 'rollup-plugin-postcss';
-
 import pkg from './package.json';
 
 export default {
-  input: './src/index.js',
+  input: 'src/index.ts',
+  extensions: ['.js', '.jsx', '.ts', '.tsx'],
   output: [
     {
       file: pkg.main,
       format: 'cjs',
+      sourcemap: true,
       exports: 'named',
     },
-    {
-      file: pkg.module,
-      format: 'esm',
-      exports: 'named',
-    },
+    // {
+    //   file: pkg.module,
+    //   format: 'esm',
+    //   exports: 'named',
+    // },
   ],
   external: [
-    'react',
-    'react-dom',
-    'react-dnd',
-    'prop-types',
-    'react-dnd-html5-backend',
-    'frontend-collective-react-dnd-scrollzone',
-    'react-virtualized',
-    'lodash.isequal',
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
   ],
+  // external: [
+  //   'react',
+  //   'react-dom',
+  //   'react-dnd',
+  //   'prop-types',
+  //   'react-dnd-html5-backend',
+  //   'frontend-collective-react-dnd-scrollzone',
+  //   'react-virtualized',
+  //   'lodash.isequal',
+  // ],
   plugins: [
-    nodeResolve(),
+    typescript({
+      typescript: require('typescript'),
+      check: false,
+    }),
+    //    nodeResolve(),
     postcss({ name: './style.css' }),
-    commonjs({
-      include: 'node_modules/**',
-    }),
-    babel({
-      exclude: 'node_modules/**',
-    }),
+    // commonjs({
+    //   include: 'node_modules/**',
+    // }),
+    // babel({
+    //   exclude: 'node_modules/**',
+    // }),
   ],
 };
